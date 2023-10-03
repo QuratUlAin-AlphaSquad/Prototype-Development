9e2adb321c2ef96b417e7fc76bfda9992050ef32
QuratUlAin <quratulain.alphasquad@gmail.com>
2023-10-03 21:51:12 +0500

    PD Task Changes

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+import logo from '../assets/WF Logo Placeholder.png'
+
+const Header = () => {
+  return (
+    <div className=" px-10 py-6">
+    {/* <p>Top Bar</p> */}
+    <img src={logo}></img>
+  </div>
+  )
+}
+
+export default Header
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+const Header = () => {
+    return (
+      <div className="bg-neutral-500 w-[260px] h-[884px]">
+      <p>Side bar</p>
+    </div>
+    )
+  }
+  
+  export default Header
